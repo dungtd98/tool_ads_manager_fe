44a5c54e99eb241e7d0c1db13010231adb93d422
Trương Đức Dũng <dungtd@relipasoft.com>
2024-10-11 15:27:15 +0700

    update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
-// Load environment variables from .env file
-// require('dotenv').config();
+// Console log để kiểm tra biến môi trường
 console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL);
+
+// Định nghĩa giá trị mặc định
+const DEFAULT_API_URL = 'https://tooladsmanagerbe-production-api.up.railway.app';
+
+// Tạo một instance của Axios với baseURL được thiết lập
 const axiosInstance = axios.create({
-  
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL || DEFAULT_API_URL, // Sử dụng REACT_APP_BASE_URL nếu có, nếu không thì sử dụng DEFAULT_API_URL
   headers: {
     'Content-Type': 'application/json',
   },
